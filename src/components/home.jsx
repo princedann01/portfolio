16c4d63a71b2082869f14829e59aa72af4019b72
princedann01 <princedann@hotmail.com>
2024-04-15 12:57:04 +0200

    i did change

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -34,7 +34,7 @@ function Home() {
       <div className="flex-grow flex justify-center items-center bg-neutral-200">
         <div className="flex flex-col justify-center items-center text-center md:mx-16 lg:mx-24 xl:mx-32">
           <h1 className="text-5xl font-bold mb-4 md:mb-6 lg:mb-8">SIBO Daniel</h1>
-          <h2 className=" text-2xl mb-6 md:mb-8 lg:mb-10">I'm a professional Full Stack Developer &amp; UI/UX Designer and electronics and telcommunication engineer</h2>
+          <h2 className=" text-2xl mb-6 md:mb-8 lg:mb-10">I&apos;m a professional Full Stack Developer &amp; UI/UX Designer and electronics and telcommunication engineer</h2>
           <button className="rounded-full h-12 w-48 bg-gray-400 hover:bg-gray-500 text-white font-semibold tracking-wide transition duration-300 ease-in-out">
             <a href="/about">ABOUT ME</a>
           </button>
